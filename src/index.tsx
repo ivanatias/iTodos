65b74b3a63a8837a449c3a6bdb925dba0252de56
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-18 00:37:01 -0400

    feat: add toaster

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import './index.css'
 import App from './App'
 
@@ -9,6 +10,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster />
     </BrowserRouter>
   </React.StrictMode>
 )
