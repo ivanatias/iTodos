dcdb6e4ebcea33206cef233ace7df2dd7e6e8f7e
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-23 19:05:55 -0400

    refactor(useField): refactor custom hook to handle checkboxes

--- src/hooks/useField.ts
+++ src/hooks/useField.ts
@@ -2,22 +2,28 @@ import { useState } from 'react'
 import type { InputChangeEvent } from '../models/types'
 
 interface UseFieldType {
-  type: 'text' | 'password'
+  type: 'text' | 'password' | 'checkbox'
 }
 
 export const useField = ({ type }: UseFieldType) => {
   const [value, setValue] = useState('')
+  const [checked, setChecked] = useState(false)
 
   const onChange = (e: InputChangeEvent) => {
+    if (type === 'checkbox') setChecked(e.target.checked)
+
     setValue(e.target.value)
   }
 
   const reset = () => {
+    if (type === 'checkbox') setChecked(false)
+
     setValue('')
   }
 
   return {
     value,
+    checked,
     type,
     onChange,
     reset,
