32090bdbc14f045e5a073201acca72bc8e6f702b
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-23 19:07:07 -0400

    feat: add main page

---
+++ src/pages/main.tsx
@@ -0,0 +1,84 @@
+import Input from '../components/input'
+import { useField } from '../hooks/useField'
+import { useAuth } from '../contexts/auth-context'
+import { useNavigate } from 'react-router'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { addTodo } from '../services/add-todo'
+import { getUserTodos } from '../services/get-user-todos'
+import { toast } from 'react-hot-toast'
+
+const Main = () => {
+  const { user, logoutUser } = useAuth()
+  const { data: todos } = useQuery(
+    ['todos'],
+    async () => await getUserTodos(user?.token as string)
+  )
+  const { mutate: addNewTodo } = useMutation(addTodo, {
+    onError: (error) => {
+      if (error instanceof Error) toast.error(error.message)
+    },
+  })
+  const { reset: resetTodoInput, ...todoText } = useField({ type: 'text' })
+  const { reset: resetTodoPriority, ...todoPriority } = useField({
+    type: 'checkbox',
+  })
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logoutUser()
+    navigate('/login', { replace: true })
+  }
+
+  const createTodo = () => {
+    if (user === null) return
+    addNewTodo({
+      title: todoText.value,
+      isPriority: todoPriority.checked,
+      token: user.token,
+    })
+    resetTodoInput()
+    resetTodoPriority()
+  }
+
+  return (
+    <section className='flex flex-col w-full max-w-md gap-5'>
+      <div className='flex flex-wrap items-center justify-center gap-4'>
+        <h1 className='text-xl font-bold 2xl:text-2xl'>Hello, {user?.name}</h1>
+        <button
+          onClick={handleLogout}
+          className='text-sm font-bold text-red-500 underline border-none outline-none underline-offset-2 2xl:text-base'
+        >
+          Logout
+        </button>
+      </div>
+      <div className='flex flex-col gap-3'>
+        <div className='flex items-center justify-between gap-3'>
+          <Input {...todoText} id='todo' placeholder='Write a todo!' />
+          <button
+            className='flex-shrink-0 text-sm font-bold text-blue-500 underline border-none outline-none underline-offset-2 2xl:text-base'
+            onClick={createTodo}
+          >
+            Add todo
+          </button>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Input
+            {...todoPriority}
+            id='todoCheckbox'
+            checked={todoPriority.checked}
+          />
+          <label className='text-xs' htmlFor='todoCheckbox'>
+            Important?
+          </label>
+        </div>
+        <div>
+          {todos?.map((todo) => (
+            <p key={todo.id}>{todo.title}</p>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Main
