1fbe1ffe2e2ff55ef0630e3eac681bb848f352d9
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-23 19:04:17 -0400

    feat: add get user todos service

---
+++ src/services/get-user-todos.ts
@@ -0,0 +1,21 @@
+import type { Todo } from '../models/types'
+import { API_URL } from '../utils/constants'
+
+export const getUserTodos = async (
+  token: string
+): Promise<Todo[] | undefined> => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token} `,
+    },
+  }
+  const response = await fetch(`${API_URL}/api/todos`, config)
+
+  if (!response.ok) {
+    const errorText = await response.text()
+    throw new Error(errorText)
+  }
+
+  const todos = (await response.json()) as Todo[]
+  return todos
+}
