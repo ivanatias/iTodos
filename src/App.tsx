86885f3b519797fe60d7bf7c1efe29573874fe9e
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-23 19:06:46 -0400

    feat: include routes on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import AuthProvider from './contexts/auth-context'
 import Login from './pages/login'
+import Main from './pages/main'
 import Layout from './components/layout'
+import ProtectedRoute from './components/protected-route'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <AuthProvider>
-      <Layout>
-        <Login />
-      </Layout>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route element={<ProtectedRoute />}>
+            <Route path='/' element={<Main />} />
+          </Route>
+          <Route path='/login' element={<Login />} />
+        </Route>
+      </Routes>
     </AuthProvider>
   )
 }
