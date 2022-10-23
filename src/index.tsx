87f559e0364c61aa094f45a5eef969bb148c71f9
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-23 17:22:44 -0400

    feat: add query client

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'react-hot-toast'
 import './index.css'
 import App from './App'
 
+const queryClient = new QueryClient()
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+        <ReactQueryDevtools />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 )
