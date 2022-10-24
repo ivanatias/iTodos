997fe24e2caf90195a246b361df5ca8779f8cac0
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-24 01:31:54 -0400

    refactor: handle query and mutation errors in queryClient
    query errors will be handled on queryClient if query cache is populated to keep UI stale. Mutation errors will be handled on queryClient as well.

--- src/hooks/useMutateTodos.ts
+++ src/hooks/useMutateTodos.ts
@@ -1,14 +1,10 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { addTodo } from '../services/add-todo'
-import { toast } from 'react-hot-toast'
 import type { Todo } from '../models/types'
 
 export const useMutateTodos = (token: string) => {
   const queryClient = useQueryClient()
   const { mutate: addNewTodo } = useMutation(addTodo, {
-    onError: (error) => {
-      if (error instanceof Error) toast.error(error.message)
-    },
     onSuccess: (newTodo) => {
       const oldQueryData = queryClient.getQueryData(['todos', token]) as Todo[]
       queryClient.setQueryData(['todos', token], [...oldQueryData, newTodo])
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryCache,
+  MutationCache,
+} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 import './index.css'
 import App from './App'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      if (query.state.data !== undefined && error instanceof Error) {
+        toast.error(
+          `Something went wrong retrieving your todos: ${error.message}`
+        )
+      }
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      if (error instanceof Error) toast.error(error.message)
+    },
+  }),
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
