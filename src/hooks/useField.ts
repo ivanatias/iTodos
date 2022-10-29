c2f26dd6b2c84750b73271f05c91943486abcd87
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-28 20:31:58 -0400

    refactor(useField): adapt hook to receive default values

--- src/hooks/useField.ts
+++ src/hooks/useField.ts
@@ -1,9 +1,19 @@
 import { useState } from 'react'
 import type { InputChangeEvent, InputType } from '../models/types'
 
-export const useField = (type: InputType) => {
-  const [value, setValue] = useState('')
-  const [checked, setChecked] = useState(false)
+interface UseField {
+  type: InputType
+  defaultTextValue?: string
+  defaultCheckedValue?: boolean
+}
+
+export const useField = ({
+  type,
+  defaultTextValue,
+  defaultCheckedValue,
+}: UseField) => {
+  const [value, setValue] = useState<string>(defaultTextValue ?? '')
+  const [checked, setChecked] = useState<boolean>(defaultCheckedValue ?? false)
 
   const onChange = (e: InputChangeEvent) => {
     if (type === 'checkbox') setChecked(e.target.checked)
