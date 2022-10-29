2e4a10d6c899e60ef0777eb408bc698124c48ac6
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-28 20:55:40 -0400

    fix(TodoItem): remove unneeded key attribute

--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -21,7 +21,7 @@ const TodoItem = ({ todo }: Props) => {
   }
 
   return (
-    <div key={todo.id} className='flex flex-col gap-1 p-2 bg-white rounded-sm'>
+    <div className='flex flex-col gap-1 p-2 bg-white rounded-sm'>
       <div className='flex items-center justify-end gap-3'>
         <button className='p-1 font-bold text-white bg-green-500 border-none rounded-md outline-none'>
           <CheckIcon className='w-3 h-3' />
