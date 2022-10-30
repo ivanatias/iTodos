c9a157d52b3054a418996c821b5b79b2c371a9db
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-29 20:03:11 -0400

    feat(useMutateTodos): add edit todo functionality with optimistic updates

--- src/hooks/useMutateTodos.ts
+++ src/hooks/useMutateTodos.ts
@@ -1,9 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { addTodo } from '../services/add-todo'
+import { editTodo } from '../services/edit-todo'
 import type { Todo } from '../models/types'
 
-export const useMutateTodos = (token: string) => {
+interface UseMutateTodos {
+  token: string
+  todoId?: string
+}
+
+export const useMutateTodos = ({ token, todoId }: UseMutateTodos) => {
   const queryClient = useQueryClient()
+
   const { mutate: addNewTodo } = useMutation(addTodo, {
     onSuccess: (newTodo) => {
       const oldQueryData = queryClient.getQueryData(['todos', token]) as Todo[]
@@ -11,5 +18,38 @@ export const useMutateTodos = (token: string) => {
     },
   })
 
-  return { addNewTodo }
+  const { mutate: modifyTodo } = useMutation(editTodo, {
+    onMutate: async ({ title, isPriority, isCompleted }) => {
+      await queryClient.cancelQueries(['todos', token])
+
+      const oldTodos = queryClient.getQueryData(['todos', token]) as Todo[]
+
+      queryClient.setQueryData(
+        ['todos', token],
+        oldTodos.map((oldTodo) => {
+          if (oldTodo.id === todoId) {
+            return {
+              ...oldTodo,
+              title,
+              isPriority,
+              isCompleted,
+            }
+          }
+          return oldTodo
+        })
+      )
+
+      return { oldTodos }
+    },
+
+    onError: (_error, _variables, context) => {
+      queryClient.setQueryData(['todos', token], context?.oldTodos)
+    },
+
+    onSettled: async () => {
+      await queryClient.invalidateQueries(['todos', token])
+    },
+  })
+
+  return { addNewTodo, modifyTodo }
 }
