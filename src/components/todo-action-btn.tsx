d80aa638e438d81488242582b2f4ab6c8482aa3a
Ivan Atias <ivan.d.atias@gmail.com>
2022-11-07 16:55:39 -0400

    feat: add disabled prop to todo action btn

--- src/components/todo-action-btn.tsx
+++ src/components/todo-action-btn.tsx
@@ -4,13 +4,20 @@ interface Props {
   Icon: SVGIcon
   actionFunction: () => void
   backgroundColor: string
+  disabled?: boolean
 }
 
-const TodoActionButton = ({ Icon, actionFunction, backgroundColor }: Props) => {
+const TodoActionButton = ({
+  Icon,
+  actionFunction,
+  backgroundColor,
+  disabled,
+}: Props) => {
   return (
     <button
       onClick={actionFunction}
       className={`p-1 font-bold text-white ${backgroundColor} border-none rounded-md outline-none`}
+      disabled={disabled}
     >
       <Icon className='w-3 h-3' />
     </button>
